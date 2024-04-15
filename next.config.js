f39d0fc58b9e335e0137ba26a414711b5a446b9f
EdgarNegrin <alu0101210964@ull.edu.es>
2024-04-15 17:53:48 +0100

    modify nextjs workflow

--- next.config.js
+++ next.config.js
@@ -14,6 +14,18 @@
 const nextConfig = {
   output: 'export',
   distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/about',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
